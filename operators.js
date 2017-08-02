74f5fa7f3d81ad1c8c1a395bb6507c0e9d070710
Technohacker <technohacker1995@gmail.com>
2017-08-02 15:48:05 +0530

    Add addition operator

--- operators.js
+++ operators.js
@@ -7,12 +7,26 @@ module.exports = {
         let jumpTo = stack.pop(),
             numArgs = stack.pop();
 
-        for (var i = 0; i < numArgs; i++) {
+        for (var i = 0; i < numArgs; i += 1) {
             console.log(stack.pop());
         }
         return {
             jumpTo,
             stack
         };
+    },
+    add(stack) {
+        let jumpTo = stack.pop(),
+            numArgs = stack.pop(),
+            sum = 0;
+
+        for (var i = 0; i < numArgs; i += 1) {
+            sum += stack.pop();
+        }
+        stack.push(sum);
+        return {
+            jumpTo,
+            stack
+        }
     }
 };
